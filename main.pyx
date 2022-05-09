1e67e2a79e85a54c453d0e98255cc788fe9ed9b6
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-05-09 17:21:12 +0430

    Fixing an indentation mistake

--- main.pyx
+++ main.pyx
@@ -18,9 +18,9 @@ def prime_finder_python(amount):
         for x in primes:
             if number % x == 0:
                 break
-            else:
-                primes.append(number)
-                found += 1
+        else:
+            primes.append(number)
+            found += 1
     return primes
 
 
@@ -40,9 +40,9 @@ def prime_finder_cython(int amount):
         for x in primes[:found]:
             if number % x == 0:
                 break
-            else:
-                primes[found] = number
-                found += 1
+        else:
+            primes[found] = number
+            found += 1
         number += 1
     return_list = [p for p in primes[:found]]
     return return_list
