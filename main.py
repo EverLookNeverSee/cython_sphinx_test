9ce665a427d852e144f1b1e057edce24dc55d967
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-05-09 16:37:27 +0430

    Implementing prime_finder_python function

--- main.py
+++ main.py
@@ -3,3 +3,22 @@
     implemented using pure python and cython
     @author: Milad Sadeghi DM - EverLookNeverSee@GitHub
 """
+
+
+def prime_finder_python(amount):
+    """
+    Prime numbers finder implemented using pure python
+    :param amount: An integer
+    :return: List of primes
+    """
+    found = 0
+    number = 2
+    primes = list()
+    while found < amount:
+        for x in primes:
+            if number % x == 0:
+                break
+            else:
+                primes.append(number)
+                found += 1
+    return primes
